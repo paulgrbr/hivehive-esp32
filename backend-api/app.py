26ab66894304b7960fd940ed042803506e54687f
paulgrbr <paulgroeber2004@gmail.com>
2025-11-03 23:26:05 +0100

    Added preview route

--- backend-api/app.py
+++ backend-api/app.py
@@ -1,27 +1,21 @@
 from flask import Flask, jsonify, request
-from routes.example import example_route
+from routes.preview import preview_route, push_frame
 import os
 from circle_detection.detect_circle import detect_circles
 
 app = Flask(__name__)
 
 # Register other routes
-app.register_blueprint(example_route)
-app.config["UPLOAD_FOLDER"] = os.path.abspath("./circle_detection/images")
+app.register_blueprint(preview_route)
 
 # Ensure the upload folder exists
-if not os.path.exists(app.config["UPLOAD_FOLDER"]):
-    os.makedirs(app.config["UPLOAD_FOLDER"])
-
-
-@app.get("/")
-def index():
-    return jsonify(message="Hello HiveHive!"), 200
+app.config["UPLOAD_FOLDER"] = os.path.abspath(
+    "backend-api/circle_detection/images")
+os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
 
 @app.post("/upload")
 def upload_image():
-
     if "image" not in request.files:
         return jsonify({"error": "No image file provided"}), 400
 
@@ -29,20 +23,16 @@ def upload_image():
     if image.filename == "":
         return jsonify({"error": "No selected file"}), 400
 
-    filename = image.filename
-    file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+    file_path = os.path.join(app.config["UPLOAD_FOLDER"], image.filename)
     image.save(file_path)
 
-    circles = detect_circles(file_path)
-
-    return (
-        jsonify(
-            {"message": f"Image {filename} uploaded successfully", "circles": circles}
-        ),
-        200,
-    )
+    circles, result_img = detect_circles(file_path)
+    push_frame(result_img)
+    return jsonify({
+        "message": f"Image {image.filename} uploaded successfully",
+        "circles": circles
+    }), 200
 
 
-# Start server
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4444, debug=True)
