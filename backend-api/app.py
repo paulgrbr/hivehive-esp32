71e5c64ed3a4aacaab924685570299b1e094545d
eliaspfe <elias.pfeiffer@gmx.de>
2025-11-17 14:25:37 +0100

    get information about all detected circles in new route

--- backend-api/app.py
+++ backend-api/app.py
@@ -7,12 +7,14 @@ from routes.preview import preview_route, push_frame
 
 app = Flask(__name__)
 
+# Global variable to store detected circles
+circles_array = []
+
 # Register other routes
 app.register_blueprint(preview_route)
 
 # Ensure the upload folder exists
-app.config["UPLOAD_FOLDER"] = os.path.abspath(
-    "backend-api/circle_detection/images")
+app.config["UPLOAD_FOLDER"] = os.path.abspath("backend-api/circle_detection/images")
 os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
 
@@ -29,10 +31,32 @@ def upload_image():
     image.save(file_path)
 
     circles, result_img = detect_circles(file_path)
+    circles_array.clear()
+    circles_array.append(circles)
     push_frame(result_img)
-    return jsonify(
-        {"message": f"Image {image.filename} uploaded successfully", "circles": circles}
-    ), 200
+    return (
+        jsonify(
+            {
+                "message": f"Image {image.filename} uploaded successfully",
+                "circles": circles,
+            }
+        ),
+        200,
+    )
+
+
+# route to get array of detected circles
+@app.get("/result")
+def get_result():
+
+    return (
+        jsonify(
+            {
+                "circles": circles_array,
+            }
+        ),
+        200,
+    )
 
 
 if __name__ == "__main__":
